67549e2bcba0e60d7217556496c83bb4564acc53
bda-mota <bea.mota04@gmail.com>
2023-11-02 12:32:28 -0300

    upload lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,4 +12,27 @@
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list 	*new_lst;
+	t_list	*node;
+	void	*temp;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	new_lst = NULL;
+	while (lst)
+	{
+		temp = f(lst->content);
+		node = ft_lstnew(temp);
+		if (!node)
+		{
+			ft_lstclear(&new_lst, (*del));
+			del(temp);
+			return (new_lst);
+		}
+		ft_lstadd_back(&new_lst, node);
+		lst = lst->next;
+	}
+	return (new_lst);
+}
