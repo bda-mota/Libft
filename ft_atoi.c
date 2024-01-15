968387acfe7242060722628238b975aaa49f999d
Beatriz <142101469+bda-mota@users.noreply.github.com>
2024-01-15 12:39:00 -0300

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -21,7 +21,7 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	sign = 1;
 	result = 0;
-	while (nptr[i] == ' ' || nptr[i] >= 9 && nptr[i] <= 13)
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
 	if (nptr[i] == '-')
 		sign = -1;
